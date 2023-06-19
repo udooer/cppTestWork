cd145d235cf1897e213f3789e7514806ff3531ea
Shane_Hsu <Shane_Hsu@asus.com>
2023-06-19 19:14:09 +0800

    Add WriteFile and ReadFile func in utiltiy
    1. Using fstream
    
    Change-Id: I0d52529f4882dab8a4fd5bbc77cef4bb12390eb0

--- Utility/WriteFile/WriteFile.cpp
+++ Utility/WriteFile/WriteFile.cpp
@@ -1,5 +1,7 @@
 #include "WriteFile.h"
 #include <cstdio>
+#include <fstream>
+#include <iostream>
 
 void shaneutility::WriteFile_C(const char* filePath, const char* content, const char* mode) {
     FILE* file = fopen(filePath, mode);
@@ -11,5 +13,61 @@ void shaneutility::WriteFile_C(const char* filePath, const char* content, const 
     else 
     {
         printf("Failed to open the file at %s\n", filePath);
+    }
+}
+
+void shaneutility::WriteFile_CPP(const std::string& filePath, const std::string& content) 
+{
+    std::fstream file;
+    file.open(filePath, std::fstream::out | std::fstream::app);
+
+    if (file.is_open()) 
+    {
+        file << content;
+        file.close();
+    }
+}
+
+void shaneutility::WriteFile_CPP(const std::string& filePath, const std::wstring& content)
+{
+    std::wfstream file;
+    file.open(filePath, std::wfstream::out | std::wfstream::app);
+
+    if (file.is_open()) 
+    {
+        file << content;
+        file.close();
+    }
+}
+
+void shaneutility::ReadFile_CPP(const std::string& filePath) 
+{
+    std::fstream file;
+    file.open(filePath, std::fstream::in);
+
+    if (file.is_open()) 
+    {
+        std::string line;
+        while (std::getline(file, line)) 
+        {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    }
+}
+
+void shaneutility::WReadFile_CPP(const std::string& filePath)
+{
+    std::wfstream file;
+    file.open(filePath, std::wfstream::in);
+
+    if (file.is_open()) 
+    {
+        std::wstring line;
+        while (std::getline(file, line)) 
+        {
+            std::wcout << line << std::endl;
+        }
+        file.close();
     }
 }
--- Utility/WriteFile/WriteFile.h
+++ Utility/WriteFile/WriteFile.h
@@ -1,4 +1,13 @@
 #pragma once 
+#include <string>
 namespace shaneutility {
 	void WriteFile_C(const char* filePath, const char* content, const char* mode = "a");
+
+	void WriteFile_CPP(const std::string& filePath, const std::string& content);
+
+	void WriteFile_CPP(const std::string& filePath, const std::wstring& content);
+
+	void ReadFile_CPP(const std::string& filePath);
+
+	void WReadFile_CPP(const std::string& filePath);
 }
